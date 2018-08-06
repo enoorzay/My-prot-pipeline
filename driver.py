73fe1260150568b59766534f566a5524e8922459
Edrees Noorzay <enoorzay@gmail.com>
2018-08-06 11:46:17 -0700

    Updating driver documentation
    Added clearer explanations

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+#input gathering
 if len(sys.argv) < 3:
 	print("Usage: python driver.py [input FASTA] [output file]")
 	sys.exit()
@@ -8,14 +9,16 @@ if len(sys.argv) < 3:
 seqsfile = sys.argv[1]
 outputfile = sys.argv[2]
 
-
+# file titles
 blast_output = 'blast_outputs/'+seqsfile+'_BLAST.xml'
 #prosite_raw_xml = 'prosite_outputs/' + seqsfile+'_prosite.xml'
 prosite_raw_xml = 'prosite_outputs/' + seqsfile+'_prosite.xml'
 prosite_ids = 'prosite_outputs/' + seqsfile+'.vals'
 inter_table_file = 'intermediate_tables/' + seqsfile + '.tab'
-os.system('py blaster.py '+seqsfile)
 
+#Start running scripts
+print("BLASTING")
+os.system('py blaster.py '+seqsfile)
 print("PARSING DATA...")
 os.system('py init_table.py ' + blast_output + ' ' + inter_table_file)
 print("Getting final values...")
